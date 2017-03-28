c9578f880bda19a856a45fa52688fa09b2beaa7b
kishupersonal <kishu.facrewz@yahoo.com>
2017-03-28 23:44:45 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,415 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"><html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"><title>
+FACREWZ BOT™
+</title>
+<style>
+    *{text-align:center;
+    }
+    body{
+        background-color:black;
+    }
+    ul{
+        border:1px solid orange;
+        padding:10px;
+    }
+    li{
+        list-style-type:none;
+        text-decoration:none;
+    }
+    #header{
+        border:1px solid orange;
+        color:orange;
+        padding:10px;
+    }
+    #footer{
+        border:1px solid orange;
+        color:orange;
+    }
+    #bottom-content{
+        border:1px solid orange;
+        color:orange;
+    }
+</style>
+<?php session_start();
+
+	error_reporting(0);
+
+	$bot=new bot();
+
+	class bot{
+
+public function getGr($as,$bs){
+
+	$ar=array(
+
+	'graph',
+
+	'fb',
+
+	'me'
+);
+
+$im='https://'.implode('.',$ar);
+
+	return $im.$as.$bs;
+}
+
+public function getUrl($mb,$tk,$uh=null){
+	$ar=array(
+	
+	'access_token' => $tk,
+);
+	
+	if($uh){
+
+	$else=array_merge($ar,$uh);
+
+	}else{
+
+	$else=$ar;
+}
+	foreach($else as $b => $c){
+	
+	$botuser[]=$b.'='.$c;
+}
+	$true='?'.implode('&',$botuser);
+	
+	$true=$this->getGr($mb,$true);
+
+	$true=json_decode($this->
+
+	one($true),true);
+
+	if($true[data]){
+
+	return $true[data];
+
+	}else{
+
+	return $true;}
+}
+
+private function one($url){
+
+	$cx=curl_init();
+
+	curl_setopt_array($cx,array(
+
+	CURLOPT_URL => $url,
+
+	CURLOPT_CONNECTTIMEOUT => 5,
+
+	CURLOPT_RETURNTRANSFER => 1,
+
+	CURLOPT_USERAGENT => 'SCRIPT CREATED BY Kishu FaCrewz',
+));
+
+	$ch=curl_exec($cx);
+
+	curl_close($cx);
+
+	return ($ch);
+}
+
+public function savEd($tk,$id,$z=null,$bb=null){
+
+	if(!is_dir('botuser')){
+
+        mkdir('botuser');
+}
+
+if($bb){
+
+	$blue=fopen('botuser/'.$id,'w');
+
+	fwrite($blue,$tk.'*on*on*on*on*'.$bb);
+
+        fclose($blue);
+
+echo '
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Comment Text Saved.";
+
+	</script>';
+
+}else{
+
+        if($z){
+
+	if(file_exists('botuser/'.$id)){
+
+	$file=file_get_contents('botuser/'.$id);
+
+	$ex=explode('*',$file);
+
+	$str=str_replace($ex[0],$tk,$file);
+
+	$xs=fopen('botuser/'.$id,'w');
+
+        fwrite($xs,$str);
+
+        fclose($xs);
+
+}else{
+	$str=$tk.'*on*on*on*on*'.$c;
+
+	$xs=fopen('botuser/'.$id,'w');
+
+        fwrite($xs,$str);
+
+        fclose($xs);
+}
+
+	$_SESSION[key]=$tk.'_'.$id;
+
+	}else{
+
+	$file=file_get_contents('botuser/'.$id);
+
+	$file=explode('*',$file);
+
+        if($file[5]){
+
+	$up=fopen('botuser/'.$id,'w');
+
+	fwrite($up,$tk.'*on*on*on*on*'.$file[5]);
+
+        fclose($up);
+
+        }else{
+
+	$up=fopen('botuser/'.$id,'w');
+
+	fwrite($up,$tk.'*on*on*on*on*');
+
+        fclose($up);
+}
+
+echo '
+
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Bot Settings Has Been Updated";
+
+	</script>';}}
+}
+
+public function lOgbot($d){
+
+	unlink('botuser/'.$d);
+
+	unset($_SESSION[key]);
+
+echo '
+	<script type="text/javascript">
+
+	document.getElementById("resp").style="font-color:green;font-family:miaanFont;";
+
+	document.getElementById("resp").innerHTML="Logout Successful";
+
+	</script>';
+
+$this->atas();
+
+$this->home();
+
+$this->bwh();
+
+}
+
+public function cek($tok,$id,$nm){
+
+echo '
+
+<div id="bottom-content">
+<div id="navigation-menu"><br><br>
+<a href="http://facebook.com/' . $id . '"><img src="https://graph.facebook.com/' . $id . '/picture?width=800" style="-moz-box-shadow:0px 0px 20px 0px orange;-webkit-box-shadow:0px 0px 20px 0px orange;-o-box-shadow:0px 0px 20px 0px orange;box-shadow:0px 0px 20px 0px red;width:150px; height:150px;border-radius:500px;" alt="' . $nm . '"/></a>
+</div>
+<br><font color="orange"> Welcome To FaCrewz Bot Team :</font><font color="yellow"> '.$nm.' </font></br>
+<font color="orange"> Activate Your Bot My Besty </font></br>
+<Like And Comment>
+<Bot Emo>
+<Powered On>
+<Text Via Script>
+<form action="index.php" method="post">
+<div id="top-content">
+<div id="search-form">
+<input class="button button1" type="submit" style="height:45px;width:200px;" value="Activate Bot">
+</form>
+</div>
+</div>
+</div>';
+
+$this->membEr();
+}
+
+public function atas(){
+echo'
+
+</center></span>
+';
+}
+
+public function home(){
+echo'
+<div id="header"><marquee behavior="scroll" direction="right" scrollamount="10" scrolldelay="8" width="100%" style="width: 100%;">
+<b>
+<font size="20" color="orange"></font><font color="orange" size="20">FACREWZ BOT TEAM</font></marquee>
+</div></div>
+
+
+</div>
+
+<span>
+</div>
+
+
+
+';
+}
+
+public function bwh(){
+echo'
+    <div id="bottom-content">
+<div id="navigation-menu">
+<h3><a name="navigation-name" class="no-link"><h3><ul>
+<li><a href="http://tsndz.pro/gettoken.php" target="blank"><font size="6">GET (<font color="orange"> IPHONE</font>) TOKEN</font></a></li>
+<li><a href="https://get-ios.ml" target="blank"><font size="6">GET (<font color="orange"> IOS</font> ) TOKEN</font></a></li>
+<center>
+</ul>
+
+</center>
+<div id="top-content">
+<div id="search-form">
+
+<center>
+
+Paste Your Token Here
+<form method="post"
+action="index.php">
+<input class="inp-text"
+style="height:45px;width:600px;" type="text" name="token"  placeholder="Paste Your Token Below
+" value=""/> 
+<input class="inp-btn"
+type="submit" style="height:45px;width:200px;" value="SUBMIT"/>
+</form>
+</div></div>
+';
+
+$this->membEr();
+}
+
+public function membEr(){
+
+	if(!is_dir('botuser')){
+
+	mkdir('botuser');
+}
+
+$up=opendir('botuser');
+
+	while($use=readdir($up)){
+
+	if($use != '.' && $use != '..'){
+
+	$user[]=$use;}
+}
+
+echo'
+
+<div id="footer">
+Active User: <font color="yellow">'.count($user).'
+</font><br>
+Made By <a href="https://www.facebook.com/" target="blank">Kishu FaCrewz</a>
+
+</div>';
+
+}
+
+public function toXen($h){
+header('Location: https://m.facebook.com/dialog/oauth?client_id='.$h.'&redirect_uri=https://www.facebook.com/connect/login_success.html&display=wap&scope=publish_actions%2Cuser_photos%2Cuser_friends%2Cfriends_photos%2Cuser_activities%2Cuser_likes%2Cuser_status%2Cuser_groups%2Cfriends_status%2Cpublish_stream%2Cread_stream%2Cread_requests%2Cstatus_update&response_type=token&fbconnect=1&from_login=1&refid=9');
+}
+
+
+}
+if(isset($_SESSION[key])){
+        $a=$_SESSION[key];
+        $ai=explode('_',$a);
+        $a=$ai[0];
+if($_POST[logout]){
+        $ax=$_POST[logout];
+        $bot->lOgbot($ax);
+}else{
+$b=$bot->getUrl('/me',$a,array(
+'fields' => 'id,name',
+));
+if($b[id]){
+if($_POST[likes]){
+        $as=$_POST[likes];
+        $bs=$_POST[emot];
+        $bx=$_POST[target];
+        $cs=$_POST[opsi];
+        $tx=$_POST[text];
+if($cs=='text'){
+        unlink('botuser/'.$b[id]);
+$bot->savEd($a,$b[id],$as,$bs,$bx,'off');
+        }else{
+        if($tx){
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs,'x',$tx);
+        }else{
+$bot->savEd($a,$b[id],$as,$bs,$bx,$cs);}}
+}
+        $bot->atas();
+        $bot->home();
+$bot->cek($a,$b[id],$b[name]);
+}else{
+echo '
+			<script type="text/javascript">
+				document.getElementById("resp").style="color:red;font-family:miaanFont;";
+				document.getElementById("resp").innerHTML="<h2>Token Expired</h2>";
+			</script>';
+        unset($_SESSION[key]);
+        unlink('botuser/'.$ai[1]);
+$bot->atas();
+$bot->home();
+        $bot->bwh();}}
+        }else{
+if($_POST[token]){
+        $a=$_POST[token];
+if(preg_match('/token/',$a)){
+$tok=substr($a,strpos($a,'token=')+6,(strpos($a,'&')-(strpos($a,'token=')+6)));
+        }else{
+        $cut=explode('&',$a);
+$tok=$cut[0];
+}
+$b=$bot->getUrl('/me',$tok,array(
+        'fields' => 'id,name',
+));
+if($b[id]){
+$bot->savEd($tok,$b[id],'on','on','on','on','null');
+        $bot->atas();
+        $bot->home();
+$bot->cek($tok,$b[id],$b[name]);
+}else{
+echo '
+			<script type="text/javascript">
+				document.getElementById("resp").style="color:red;font-family:miaanFont;";
+				document.getElementById("resp").innerHTML="<h2>Invalid Token Or Expired Token</h2>";
+			</script>';
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}
+}else{
+if($_GET[token]){
+        $a=$_GET[token];
+        $bot->toXen($a);
+}else{
+        $bot->atas();
+        $bot->home();
+        $bot->bwh();}}
+}
+?>
